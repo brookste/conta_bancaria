1d46627c83f4b3221a12da7c1da641461510774a
Ester Santos <esterssantos03@gmail.com>
2025-11-07 13:15:05 -0300

    Classes Conta  Poupanca e Conta Corrente adicionadas

--- menu.ts
+++ menu.ts
@@ -1,128 +1,115 @@
-import leia = require ("readline-sync")
-import { colors } from "./src/util/colors"
-import { Conta } from "./src/model/Conta";
+import leia = require("readline-sync");
+import { colors } from './src/util/colors';
+import { Conta } from './src/model/Conta';
+import { ContaCorrente } from './src/model/ContaCorrente';
+import { ContaPoupanca } from './src/model/ContaPoupança';
 
 export function main() {
 
-    let opcao: number
+    let opcao: number;
+    
+    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
+    conta.visualizar();
+    conta.sacar(10500);
+    conta.visualizar();
+    conta.depositar(5000);
+    conta.visualizar();
 
-    const conta: Conta = new Conta (1, 108, 1, "Ester", 19000)
-    conta.visualizar()
-    conta.sacar(10200)
-    conta.visualizar()
-    conta.depositar(10000)
-    conta.visualizar()
+    const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
+    contacorrente.visualizar();
+    contacorrente.sacar(2000);
+    contacorrente.visualizar();
+    contacorrente.depositar(1000);
+    contacorrente.visualizar();
 
+    const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 108, 2, "Ester", 1000, 10);
+    contapoupanca.visualizar();
+    contapoupanca.sacar(200);
+    contapoupanca.visualizar();
+    contapoupanca.depositar(1000);
+    contapoupanca.visualizar();
 
     while (true) {
 
         console.log(colors.bg.black, colors.fg.yellow, 
-                    "***********************************************************");
-        console.log("                                                           ")
-        console.log("                                                           ")
-        console.log("                        AURABANK                           ")
-        console.log("                                                           ")
-        console.log("***********************************************************")
-        console.log("                                                           ")
-        console.log("               1 - Criar Conta                             ")
-        console.log("               2 - Listar Todas As Contas                  ")
-        console.log("               3 - Buscar Conta Por Número                 ")
-        console.log("               4 - Atualizar Dados da Conta                ")
-        console.log("               5 - Apagar Conta                            ")
-        console.log("               6 - Sacar                                   ")
-        console.log("               7 - Depositar                               ")
-        console.log("               8 - Tranferir Valores Entre Contas          ")
-        console.log("               9 - Sair                                    ")
-        console.log("                                                           ")
-        console.log("***********************************************************")
-        console.log("                                                           ",
-        colors.reset)
+                    "*****************************************************");
+        console.log("                                                     ");
+        console.log("                     AURABANK                        ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ");
+        console.log("            1 - Criar Conta                          ");
+        console.log("            2 - Listar todas as Contas               ");
+        console.log("            3 - Buscar Conta por Numero              ");
+        console.log("            4 - Atualizar Dados da Conta             ");
+        console.log("            5 - Apagar Conta                         ");
+        console.log("            6 - Sacar                                ");
+        console.log("            7 - Depositar                            ");
+        console.log("            8 - Transferir valores entre Contas      ");
+        console.log("            9 - Sair                                 ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ", colors.reset);
 
-        console.log(colors.bg.black, colors.fg.yellow,
-            "Entre com a opcao desejada: ", colors.reset)
-        opcao = leia.questionInt("")
+        console.log("Entre com a opção desejada: ");
+        opcao = leia.questionInt("");
 
-        if(opcao == 9){
-            console.log(colors.fg.greenstrong,
-                "AuraBank - Seu Futuro Seguro e Próspero!")
-                sobre()
-                console.log(colors.reset, "")
-            process.exit(0)
+        if (opcao == 9) {
+            console.log(colors.fg.greenstrong, "\nAuraBank - Seu Futuro Seguro e Próspero!");
+            sobre();
+            console.log(colors.reset, "");
+            process.exit(0);
         }
 
-        switch(opcao){
-            
+        switch (opcao) {
             case 1:
-                console.log(colors.fg.whitestrong,
-                    "\n\nCriar conta\n\n", colors.reset)
+                console.log(colors.fg.whitestrong, "\n\nCriar Conta\n\n", colors.reset);
+                
+                keyPress()
+                break;
+            case 2:
+                console.log(colors.fg.whitestrong, "\n\nListar todas as Contas\n\n", colors.reset);
 
                 keyPress()
-                break
-
-            case 2:
-                console.log(colors.fg.whitestrong,
-                    "\n\nListar Todas As Contas\n\n", colors.reset)
-
-                keyPress()    
-                break
-
+                break;
             case 3:
-                console.log(colors.fg.whitestrong,
-                    "\n\nBuscar Conta Por Número\n\n", colors.reset)
+                console.log(colors.fg.whitestrong, "\n\nConsultar dados da Conta - por número\n\n", colors.reset);
 
-                keyPress()    
-                break
-
+                keyPress()
+                break;
             case 4:
-                console.log(colors.fg.whitestrong,
-                    "\n\nAtualizar Dados da Conta\n\n", colors.reset)
+                console.log(colors.fg.whitestrong, "\n\nAtualizar dados da Conta\n\n", colors.reset);
 
-                keyPress()    
-                break
-
+                keyPress()
+                break;
             case 5:
-                console.log(colors.fg.whitestrong,
-                    "\n\nApagar Conta\n\n", colors.reset)
+                console.log(colors.fg.whitestrong, "\n\nApagar uma Conta\n\n", colors.reset);
 
-                keyPress()    
-                break
-
+                keyPress()
+                break;
             case 6:
-                console.log(colors.fg.whitestrong,
-                    "\n\nSacar\n\n", colors.reset)
+                console.log(colors.fg.whitestrong, "\n\nSaque\n\n", colors.reset);
 
-                keyPress()    
-                break
-
+                keyPress()
+                break;
             case 7:
-                console.log(colors.fg.whitestrong,
-                    "\n\nDepositar\n\n", colors.reset)
+                console.log(colors.fg.whitestrong, "\n\nDepósito\n\n", colors.reset);
 
-                keyPress()    
-                break
-
+                keyPress()
+                break;
             case 8:
-                console.log(colors.fg.whitestrong,
-                    "\n\nTransferir Valores Entre Contas\n\n", colors.reset)
+                console.log(colors.fg.whitestrong, "\n\nTransferência entre Contas\n\n", colors.reset);
 
-                keyPress()    
-                break
-
-            case 9:
-                console.log(colors.fg.whitestrong,
-                    "\n\nSair\n\n", colors.reset)
-
-                keyPress()    
-                break
-
+                keyPress()
+                break;
             default:
-                console.log(colors.fg.whitestrong,
-                    "Opcao Invalida!", colors.reset)
+                console.log(colors.fg.whitestrong, "\nOpção Inválida!\n", colors.reset);
 
-                keyPress()    
-                break
+                keyPress()
+                break;
         }
     }
+
 }
 
 export function sobre(): void {
