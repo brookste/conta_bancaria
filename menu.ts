b6d1e120e196319a2c6edaded38a911999cf5ef6
Ester Santos <esterssantos03@gmail.com>
2025-11-12 19:42:28 -0300

    CRUD

--- menu.ts
+++ menu.ts
@@ -2,26 +2,33 @@ import leia = require("readline-sync");
 import { ContaCorrente } from './src/model/ContaCorrente';
 import { ContaPoupanca } from './src/model/ContaPoupanca';
 import { colors } from './src/util/Colors';
+import { ContaController } from "./src/controller/ContaController";
 
 export function main() {
 
-    let opcao: number;
 
-    // Objeto da Classe ContaCorrente (Teste)
-    const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
-    contacorrente.visualizar();
-    contacorrente.sacar(2000);
-    contacorrente.visualizar();
-    contacorrente.depositar(1000);
-    contacorrente.visualizar();
+    let contas: ContaController = new ContaController()
 
-    // Objeto da Classe ContaPoupanca (teste)
-    const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 123, 2, "Victor", 1000, 10);
-    contapoupanca.visualizar();
-    contapoupanca.sacar(200);
-    contapoupanca.visualizar();
-    contapoupanca.depositar(1000);
-    contapoupanca.visualizar();
+
+    let opcao, numero, agencia, tipo, saldo, limite, aniversario: number
+    let titular: string
+    const tiposContas = ['Conta Corrente', 'Conta Poupanca']
+    
+   console.log("\nCriar Contas\n");
+
+    let cc1: ContaCorrente = new ContaCorrente(contas.gerarNumero(), 123, 1, "João da Silva", 1000, 100.0);
+    contas.cadastrar(cc1);
+
+    let cc2: ContaCorrente = new ContaCorrente(contas.gerarNumero(), 124, 1, "Maria da Silva", 2000, 100.0);
+    contas.cadastrar(cc2);
+
+    let cp1: ContaPoupanca = new ContaPoupanca(contas.gerarNumero(), 125, 2, "Mariana dos Santos", 4000, 12);
+    contas.cadastrar(cp1);
+
+    let cp2: ContaPoupanca = new ContaPoupanca(contas.gerarNumero(), 125, 2, "Juliana Ramos", 8000, 15);
+    contas.cadastrar(cp2);
+
+    contas.listarTodas();
 
     while (true) {
 
@@ -58,26 +65,102 @@ export function main() {
         switch (opcao) {
             case 1:
                 console.log(colors.fg.whitestrong, "\n\nCriar Conta\n\n", colors.reset);
-                
+
+                console.log ("Digite o numero da agencia: ")
+                agencia = leia.questionInt("")
+
+                console.log("Digite o nome do titular da conta: ")
+                titular = leia.question("")
+
+                console.log("Digite o tipo da conta: ")
+                tipo = leia.keyInSelect(tiposContas, "", {cancel: false}) + 1
+
+                console.log("Digite o saldo da conta (R$): ")
+                saldo = leia.questionFloat("")
+
+                switch(tipo) {
+                    case 1:
+                        console.log("Digite o limite da conta (R$): ")
+                        limite = leia.questionFloat("")
+                        contas.cadastrar( new ContaCorrente (contas.gerarNumero(), agencia, tipo, titular, saldo, limite))
+
+                        break
+
+                    case 2:
+                        console.log("Digite o dia do aniversario da Conta Poupanca: ")
+                        aniversario = leia.questionInt("")
+                        contas.cadastrar(new ContaPoupanca (contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario))
+
+                        break
+                }
+
                 keyPress()
                 break;
+
             case 2:
                 console.log(colors.fg.whitestrong, "\n\nListar todas as Contas\n\n", colors.reset);
+
+                contas.listarTodas()
 
                 keyPress()
                 break;
             case 3:
                 console.log(colors.fg.whitestrong, "\n\nConsultar dados da Conta - por número\n\n", colors.reset);
+                console.log("Digite o numero da conta: ")
+                numero = leia.questionInt("")
+                contas.procurarPorNumero(numero)
 
                 keyPress()
                 break;
+
             case 4:
                 console.log(colors.fg.whitestrong, "\n\nAtualizar dados da Conta\n\n", colors.reset);
 
-                keyPress()
-                break;
+                console.log("Digite o número da Conta: ");
+                numero = leia.questionInt("");
+
+                let conta = contas.buscarNoArray(numero);
+
+                if (conta != null) {
+
+                    console.log("Digite o Número da agência: ");
+                    agencia = leia.questionInt("");
+
+                    console.log("Digite o Nome do Titular da conta: ");
+                    titular = leia.question("");
+
+                    tipo = conta.tipo;
+
+                    console.log("\nDigite o Saldo da conta (R$): ");
+                    saldo = leia.questionFloat("");
+
+                switch (tipo) {
+                    case 1:
+                        console.log("Digite o Limite da Conta (R$): ");
+                        limite = leia.questionFloat("");
+                        contas.atualizar( new ContaCorrente(numero, agencia, tipo, titular, saldo, limite));
+                            break;
+
+                    case 2:
+                        console.log("Digite o Dia do aniversário da Conta Poupança: ");
+                        aniversario = leia.questionInt("");
+                        contas.atualizar(new ContaPoupanca(numero, agencia, tipo, titular, saldo, aniversario));
+                            
+                    keyPress()
+                    break
+                }    
+            }
+            else {
+                    console.log(colors.fg.red, "\nA Conta numero: " + numero +
+                        " não foi encontrada!", colors.reset);
+                 }
+
             case 5:
                 console.log(colors.fg.whitestrong, "\n\nApagar uma Conta\n\n", colors.reset);
+
+                console.log("Digite o número da Conta: ");
+                numero = leia.questionInt("");
+                contas.deletar(numero);
 
                 keyPress()
                 break;
@@ -119,4 +202,4 @@ function keyPress(): void {
     console.log("\nPressione enter para continuar...");
     leia.prompt();
 }
-main();
+main()
