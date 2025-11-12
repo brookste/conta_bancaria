25eb9c878b535456ef3b1151d66915525213b0cd
Ester Santos <esterssantos03@gmail.com>
2025-11-12 19:57:00 -0300

    Finalizacao Classe Controller/CRUD

--- menu.ts
+++ menu.ts
@@ -3,7 +3,29 @@ import { colors } from "./src/util/colors"
 
 export function main() {
 
-    let opcao: number
+
+    let contas: ContaController = new ContaController()
+
+
+    let opcao, numero, agencia, tipo, saldo, limite, aniversario, valor, numeroDestino: number
+    let titular: string
+    const tiposContas = ['Conta Corrente', 'Conta Poupanca']
+    
+   console.log("\nCriar Contas\n");
+
+    let cc1: ContaCorrente = new ContaCorrente(contas.gerarNumero(), 123, 1, "João da Silva", 1000, 100.0);
+    contas.cadastrar(cc1);
+
+    let cc2: ContaCorrente = new ContaCorrente(contas.gerarNumero(), 124, 1, "Maria da Silva", 2000, 100.0);
+    contas.cadastrar(cc2);
+
+    let cp1: ContaPoupanca = new ContaPoupanca(contas.gerarNumero(), 125, 2, "Mariana dos Santos", 4000, 12);
+    contas.cadastrar(cp1);
+
+    let cp2: ContaPoupanca = new ContaPoupanca(contas.gerarNumero(), 125, 2, "Juliana Ramos", 8000, 15);
+    contas.cadastrar(cp2);
+
+    contas.listarTodas();
 
     while (true) {
 
@@ -75,33 +97,50 @@ export function main() {
                 console.log(colors.fg.whitestrong,
                     "\n\nApagar Conta\n\n", colors.reset)
 
-                keyPress()    
-                break
+                keyPress()
+                break;
 
             case 6:
-                console.log(colors.fg.whitestrong,
-                    "\n\nSacar\n\n", colors.reset)
+                console.log(colors.fg.whitestrong, "\n\nSaque\n\n", colors.reset);
 
-                keyPress()    
-                break
+                console.log("Digite o número da Conta: ");
+                numero = leia.questionInt("");
+
+                console.log("\nDigite o valor do Saque (R$): ");
+                valor = leia.questionFloat("");
+
+                contas.sacar(numero, valor);
+
+                keyPress()
+                break;
 
             case 7:
-                console.log(colors.fg.whitestrong,
-                    "\n\nDepositar\n\n", colors.reset)
+                console.log(colors.fg.whitestrong, "\n\nDepósito\n\n", colors.reset);
+
+                console.log("Digite o número da Conta: ");
+                numero = leia.questionInt("");
+
+                console.log("\nDigite o valor do Depósito (R$): ");
+                valor = leia.questionFloat("");
+
+                contas.depositar(numero, valor);
 
                 keyPress()    
                 break
 
             case 8:
-                console.log(colors.fg.whitestrong,
-                    "\n\nTransferir Valores Entre Contas\n\n", colors.reset)
+                console.log(colors.fg.whitestrong, "\n\nTransferência entre Contas\n\n", colors.reset);
 
-                keyPress()    
-                break
+                console.log("Digite o número da Conta de Origem: ");
+                numero = leia.questionInt("");
 
-            case 9:
-                console.log(colors.fg.whitestrong,
-                    "\n\nSair\n\n", colors.reset)
+                console.log("Digite o número da Conta de Destino: ");
+                numeroDestino = leia.questionInt("");
+
+                console.log("\nDigite o valor do Depósito (R$): ");
+                valor = leia.questionFloat("");
+
+                contas.transferir(numero, numeroDestino, valor);
 
                 keyPress()    
                 break
